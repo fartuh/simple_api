f02260db1ece68f35acf94b1ae0359a0358162a9
Winzmcman <nikithobbi@gmail.com>
2019-01-24 16:35:44 +0200

    0.1dev2

--- index.php
+++ index.php
@@ -20,14 +20,18 @@ $pdo = new PDO('mysql:host=localhost;dbname=api;charset=utf8', 'root', 'root');
 
 // Handling GET requests
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $url_arr = explode('/', $url);
 
     // Getting all the users
     if($url == 'users'){
         header('Content-type: application/json');
 
-        $q = $pdo->prepare('SELECT * FROM users');
-        $q->execute();
-        $data = $q->fetchall(PDO::FETCH_ASSOC);
+        $stmt = $pdo->prepare('SELECT * FROM users');
+        $result = $stmt->execute();
+        if(!$result) exit('{"message": "error"}');
+
+        $result = $data = $stmt->fetchall(PDO::FETCH_ASSOC);
+        if(!$result) exit('{"message": "error"}');
 
         echo json_encode($data);
     }
@@ -37,6 +41,23 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
         require('templates/new.php');
     }
 
+    else if($url_arr[0] == 'users' && is_numeric($url_arr[1]) && !isset($url_arr[2])){
+        $id = trim($url_arr[1]);
+
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
+        $result = $stmt->execute([$id]);
+        if(!$result) exit('{"message": "error"}');
+
+        $result = $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        if(!$result) exit('{"message": "error"}');
+
+        echo json_encode($data);
+    }
+
+    else if($url_arr[0] == 'users' && is_numeric($url_arr[1]) && $url_arr[2] == 'edit' && !isset($url_arr[3])){
+        $id = trim($url_arr[1]);
+        require('templates/edit.php');
+    }
 }
 
 // Handling POST requests
@@ -53,5 +74,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         if(!$result) echo '{"message": "error"}';
         else echo '{"message": "user is added"}';
     }
+
 
 }
---
+++ templates/edit.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Edit the data of user number <?= $id ?></title>
+</head>
+<body>
+    <h1>User <?= $id ?> editing</h1>
+</body>
+</html>
